f5014305db9ae124560a26960cbc1b81512ff423
RAN <wangrandk@users.noreply.github.com>
2023-11-15 01:49:43 +0100

    fix

--- src/Server.java
+++ src/Server.java
@@ -11,13 +11,13 @@ public class Server extends ImplExample {
 
             // Exporting the object of implementation class
             // (here we are exporting the remote object to representative stub)
-            Operations stub = (Operations) UnicastRemoteObject.exportObject(obj, 0);
+            // Operations stub = (Operations) UnicastRemoteObject.exportObject(obj, 0);
 
             // Binding the remote object (stub) in the registry
             Registry registry = LocateRegistry.getRegistry();
 
             // The stub's primary purpose is to enable method calls on the client side to be forwarded to the remote object on the server side.
-            registry.bind("print", stub);
+            registry.bind("print", obj);
             System.err.println("Server ready");
         } catch (Exception e) {
             System.err.println("Server exception: " + e);
